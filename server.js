83ac183707d0e7f2ef094b1bede855c27d6bc883
Nasawa <keitonasawa@gmail.com>
2015-06-02 18:35:47 -0500

    Updated index.html

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/text-send.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
