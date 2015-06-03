e6407a2cc6bf4e564ecddf956116ff9cb648ffba
Nasawa <keitonasawa@gmail.com>
2015-06-02 20:03:34 -0500

    styling

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var path = require('path');
 var port = process.env.PORT || 3000;
 
-var app = express();
-
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
