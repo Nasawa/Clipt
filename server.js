8d3ca1bf16f49df4f8bc251809755b66ffa1d762
Nasawa <keitonasawa@gmail.com>
2015-06-02 19:24:15 -0500

    assets

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require('path');
 var port = process.env.PORT || 3000;
 
 //app.use("/styles", express.static(__dirname + '/styles'));
-app.use(express.static(path.join(__dirname, 'styles')));
+app.use(express.static(path.join(__dirname, 'assets')));
 app.use(express.static(path.join(__dirname, 'images')));
 /*app.use("/styles/css", express.static(__dirname + '/styles/css'));
 app.use("/styles/js", express.static(__dirname + '/styles/js'));
